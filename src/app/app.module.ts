a94a7aeb30156f1257b92b2d70c528faf25b7904
sylverengold <sylvere.foucault@gmail.com>
2023-02-03 10:12:00 +0100

    top colleagues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonHLComponent } from './button-hl/button-hl.component';
+import { ColleagueComponent } from './colleague/colleague.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonHLComponent,
+    ColleagueComponent
   ],
   imports: [
     BrowserModule
