5b6f6669262d4214608fbb021243d0815815a7ff
sylverengold <sylvere.foucault@gmail.com>
2023-02-03 16:30:24 +0100

    top colleagues

--- src/app/list-colleagues/list-colleagues.component.ts
+++ src/app/list-colleagues/list-colleagues.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Colleague} from "../colleague";
 
 @Component({
   selector: 'tc-list-colleagues',
@@ -8,18 +9,20 @@ import { Component } from '@angular/core';
 export class ListColleaguesComponent {
 
 
-
-  list_colleagues: any[] = [
-    {id: '200',name: 'Attila', score: 65, imgg: "Attila.jpg"},
-    {id: '201',name: 'Raspoutine', score: 88, imgg: "Raspoutine.jpg"},
-    {id: '202',name: 'Ghandi', score: 110, imgg: "Gandhi.jpg"},
-    {id: '203',name: 'Elvis', score: 140, imgg: "Elvis.jpg"},
-    {id: '204',name: 'Néfertiti', score: 120, imgg: "Nefertiti.jpg"},
-    {id: '205',name: 'Einstein', score: 111, imgg: "Einstein.jpg"},
-    {id: '206',name: 'Lincoln', score: 107, imgg: "Lincoln.jpg"},
-    {id: '207',name: 'Marie-Antoinette', score: 98, imgg: "Marie-Antoinette.jpg"},
-    {id: '205',name: 'Thatcher', score: 0, imgg: "Thatcher.jpg"},
-    {id: '206',name: 'Marilyn', score: 150, imgg: "Marilyn.jpg"},
-    {id: '207',name: 'Lao-Tseu', score: 98, imgg: "LaoTseu.jpg"},
+  list_colleagues: Colleague[] = [
+    {id: 200,name: 'Attila', score: 65, imgg: "Attila.jpg"},
+    {id: 201,name: 'Raspoutine', score: 88, imgg: "Raspoutine.jpg"},
+    {id: 202,name: 'Ghandi', score: 110, imgg: "Gandhi.jpg"},
+    {id: 203,name: 'Elvis', score: 140, imgg: "Elvis.jpg"},
+    {id: 204,name: 'Néfertiti', score: 120, imgg: "Nefertiti.jpg"},
+    {id: 205,name: 'Einstein', score: 111, imgg: "Einstein.jpg"},
+    {id: 206,name: 'Lincoln', score: 107, imgg: "Lincoln.jpg"},
+    {id: 207,name: 'Marie-Antoinette', score: 98, imgg: "Marie-Antoinette.jpg"},
+    {id: 208,name: 'Thatcher', score: 0, imgg: "Thatcher.jpg"},
+    {id: 209,name: 'Marilyn', score: 150, imgg: "Marilyn.jpg"},
+    {id: 210,name: 'Lao-Tseu', score: 98, imgg: "LaoTseu.jpg"},
   ]
+
+
+
 }
