11fa766151f48a573aa03dd9f991826948dfc2f5
sylverengold <sylvere.foucault@gmail.com>
2023-02-03 15:51:25 +0100

    top colleagues

--- src/app/list-colleagues/list-colleagues.component.ts
+++ src/app/list-colleagues/list-colleagues.component.ts
@@ -10,13 +10,16 @@ export class ListColleaguesComponent {
 
 
   list_colleagues: any[] = [
-    {id: '200',name: 'Attila', score: 65, imgg: "http://www.mine2sly.fr/public/Attila.jpg"},
-    {id: '201',name: 'Raspoutine', score: 88, imgg: "http://www.mine2sly.fr/public/Raspoutine.jpg"},
-    {id: '202',name: 'Ghandi', score: 110, imgg: "http://www.mine2sly.fr/public/Gandhi.jpg"},
-    {id: '203',name: 'Elvis', score: 140, imgg: "http://www.mine2sly.fr/public/Elvis.jpg"},
-    {id: '204',name: 'Néfertiti', score: 120, imgg: "http://www.mine2sly.fr/public/Nefertiti.jpg"},
-    {id: '205',name: 'Einstein', score: 111, imgg: "http://www.mine2sly.fr/public/Einstein.jpg"},
-    {id: '206',name: 'Lincoln', score: 107, imgg: "http://www.mine2sly.fr/public/Lincoln.jpg"},
-    {id: '207',name: 'Marie-Antoinette', score: 98, imgg: "http://www.mine2sly.fr/public/Marie-Antoinette.jpg"},
+    {id: '200',name: 'Attila', score: 65, imgg: "Attila.jpg"},
+    {id: '201',name: 'Raspoutine', score: 88, imgg: "Raspoutine.jpg"},
+    {id: '202',name: 'Ghandi', score: 110, imgg: "Gandhi.jpg"},
+    {id: '203',name: 'Elvis', score: 140, imgg: "Elvis.jpg"},
+    {id: '204',name: 'Néfertiti', score: 120, imgg: "Nefertiti.jpg"},
+    {id: '205',name: 'Einstein', score: 111, imgg: "Einstein.jpg"},
+    {id: '206',name: 'Lincoln', score: 107, imgg: "Lincoln.jpg"},
+    {id: '207',name: 'Marie-Antoinette', score: 98, imgg: "Marie-Antoinette.jpg"},
+    {id: '205',name: 'Thatcher', score: 0, imgg: "Thatcher.jpg"},
+    {id: '206',name: 'Marilyn', score: 150, imgg: "Marilyn.jpg"},
+    {id: '207',name: 'Lao-Tseu', score: 98, imgg: "LaoTseu.jpg"},
   ]
 }
