06f73f669cd56bb605cc57ebe4e6bede9dae0f53
sylverengold <sylvere.foucault@gmail.com>
2023-02-06 10:23:49 +0100

    top colleagues

--- src/app/list-colleagues/list-colleagues.component.ts
+++ src/app/list-colleagues/list-colleagues.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Colleague} from "../colleague";
-
+import arrayShuffle from 'array-shuffle';
 @Component({
   selector: 'tc-list-colleagues',
   templateUrl: './list-colleagues.component.html',
@@ -8,8 +8,7 @@ import {Colleague} from "../colleague";
 })
 export class ListColleaguesComponent {
 
-
-  list_colleagues: Colleague[] = [
+  list_colleagues: Colleague[] = arrayShuffle([
     {id: 200,name: 'Attila', score: 65, imgg: "Attila.jpg",old_score:[0]},
     {id: 201,name: 'Raspoutine', score: 88, imgg: "Raspoutine.jpg",old_score:[0]},
     {id: 202,name: 'Ghandi', score: 110, imgg: "Gandhi.jpg",old_score:[0]},
@@ -21,6 +20,10 @@ export class ListColleaguesComponent {
     {id: 208,name: 'Thatcher', score: 0, imgg: "Thatcher.jpg",old_score:[0]},
     {id: 209,name: 'Marilyn', score: 150, imgg: "Marilyn.jpg",old_score:[0]},
     {id: 210,name: 'Lao-Tseu', score: 98, imgg: "LaoTseu.jpg",old_score:[0]},
-  ]
+    {id: 211,name: 'Mata-Hari', score: 111, imgg: "MataHari.jpg",old_score:[0]},
+  ]);
+
+
+
 
 }
