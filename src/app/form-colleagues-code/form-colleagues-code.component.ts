0c7a9e635da6efac12cc31e876cc9e5bf892c14d
sylverengold <sylvere.foucault@gmail.com>
2023-02-09 12:21:48 +0100

    top colleagues

---
+++ src/app/form-colleagues-code/form-colleagues-code.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'tc-form-colleagues-code',
+  templateUrl: './form-colleagues-code.component.html',
+  styleUrls: ['./form-colleagues-code.component.scss']
+})
+export class FormColleaguesCodeComponent {
+
+
+ // monForm=new FormGroup({saisie:new FormControl()});
+
+}
